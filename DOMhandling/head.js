4234b1febb5a9dd38a1fe1c844d17fabb6ed63d0
Ange230700 <kouakouangeericstephane@gmail.com>
2023-08-18 09:26:44 +0200

    creating head.js for modularization

---
+++ DOMhandling/head.js
@@ -0,0 +1,3 @@
+document.write('<meta charset="UTF-8" />');
+document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0" />');
+document.write('<link rel="stylesheet" href="../style.css" type="text/css" />');
