e578cb4d97c96dadabe2460bfc0881af1ec3a10f
Ange230700 <kouakouangeericstephane@gmail.com>
2023-09-05 16:51:51 +0200

    factoring...

---
+++ includeScriptElementOfTypeJavascriptInBodyTag.js
@@ -0,0 +1,10 @@
+// LOGIC TO INCLUDE SCRIPT ELEMENTS OF TYPE JAVASCRIPT IN THE BODY
+
+function includeScriptElementOfTypeJavascriptInBodyTag(filePath) {
+    let scriptElement = document.createElement('script');
+    scriptElement.src = filePath;
+    scriptElement.type = 'text/javascript';
+    document.body.innerHTML += scriptElement.outerHTML;
+}
+
+includeScriptElementOfTypeJavascriptInBodyTag('/script1.js');
